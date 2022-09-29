6c31684f717625a884fde563f406366f8ba0d970
xuannbgch17346 <xuannbgch17346@fpt.edu.vn>
2022-09-29 01:40:57 -0700

    ok

---
+++ views/app.js
@@ -0,0 +1,21 @@
+//khai báo module (package)
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000;
+
+//set thư mục chứa views (giao diện - frontend)
+app.set("views", path.join(__dirname, "/views"));
+//set view engine (template engine)
+app.set("view engine", "hbs");
+
+//render ra trang home (index.hbs)
+//get: get method
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//chạy web server
+app.listen(port, () => {
+  console.log("Server is running at: http://localhost:3000");
+});
